3a8d82abcb5100aab38a306654fb245ced65909f
niteshkumar46 <132472966+niteshkumar46@users.noreply.github.com>
2024-12-19 14:49:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "29287896"))
+API_HASH     = environ.get("API_HASH", "b7b0c2ddd3c28c831e7411a2c66b90c1")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002389292805"))
+ADMIN        = int(environ.get("ADMIN", "7054788112"))
 CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
